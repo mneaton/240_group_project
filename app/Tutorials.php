ddeeb29c284abc2bcd3fcdfeb887851ab5fda377
Rix <rix@datto.com>
2017-11-19 21:47:20 -0500

    more routes

--- app/Tutorials.php
+++ app/Tutorials.php
@@ -5,4 +5,12 @@ namespace App;
 class Tutorials extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = "tutorials";
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function sections()
+    {
+        return $this->hasMany('App\Sections', 'foreign_key', 'tutorial_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,29 @@ Route::get('/', function () {
     return view('content.homepage');
 });
 
-Route::get('/tutorials/{tutorialId}/section/{sectionId}', function($tutorialId, $sectionId) {
+Route::get('/tutorials', function() {
+    $tutorials = \App\Tutorials::all();
 
+    return view('content.tutorials', ['tutorials' => $tutorials]);
+});
 
-    return view('content.section');
+Route::get('/tutorials/{tutorialId}/section/{sectionId}', function($tutorialId, $sectionId){
+    $section = \App\Sections::query()
+        ->where('tutorial_id', '=', $tutorialId)
+        ->where('section_id', '=', $sectionId)
+        ->get();
+
+    $tutorials = DB::table('tutorials')->join('sections', 'tutorials.id', '=', 'sections.tutorial_id')->get()->all();
+
+    return view('content.section', ['section' => $section, 'tutorials' => $tutorials]);
+});
+
+Route::get('/commands', function() {
+   $commands = DB::table('commands')
+       ->join('tutorials', 'commands.id', '=', 'tutorials.command_id')
+       ->orderBy('commands.firstLetter')
+       ->get()
+       ->all();
+
+   return view('content.commands', ['commands' => $commands]);
 });
